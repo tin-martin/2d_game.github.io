6406e278ca1ca65a5adbe3c48d90d2e610f08576
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-03-15 18:38:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let canvas = {
        clearInterval(this.game);
    }
 }
-var c = document.getElementById("canvas");
+var c = canvas;
 var ctx = c.getContext("2d");
 ctx.fillRect(20, 20, 150, 100);
 
