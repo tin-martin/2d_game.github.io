7f9717247b73fb9bdf19bfbb6629d954078c2770
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-03-15 17:29:57 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+window.addEventListener("keydown", function(event) {   switch(event.code) {
+       case "KeyS":
+       case "ArrowDown":
+           //Movement logic down
+           break;
+       case "KeyW":
+       case "ArrowUp":
+           //Movement logic up
+           break;
+       case "KeyA":
+       case "ArrowLeft":
+           //Movement logic left
+           break;
+       case "KeyD":
+       case "ArrowRight":
+           //Movement logic right
+           break;
+   }
+}, true);
+
+window.addEventListener("keyup", function(event) {
+   switch(event.code) {
+       case "KeyS":
+       case "ArrowDown":
+       case "KeyW":
+       case "ArrowUp":
+           //Movement logic vertical stop
+           break;
+       case "KeyA":
+       case "ArrowLeft":
+       case "KeyD":
+       case "ArrowRight":
