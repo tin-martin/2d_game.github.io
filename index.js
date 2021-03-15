f022ca8b036836d9be54c8782698d01a866c3a8f
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-03-15 17:58:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+let canvas = {
+   canvas: document.createElement("canvas"),
+   start: function () {
+       this.canvas.width = 800;
+       this.canvas.height = 800;
+       this.context = this.canvas.getContext("2d");
+       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+       this.coinInterval = 0;
+       this.game = setInterval(game, 20);
+   },
+   stop : function() {
+       clearInterval(this.game);
+   }
+}
+
 window.addEventListener("keydown", function(event) {   switch(event.code) {
        case "KeyS":
        case "ArrowDown":
