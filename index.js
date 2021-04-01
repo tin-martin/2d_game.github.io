25d32ba6a06b885c5375872baf510ea89e0c3b86
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-03-31 20:52:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,120 +1,78 @@
-
-//width = 800, height = 800
-let x = 10;
-let y = 750; 
-let x_vel = 5;
-let y_vel = 10;
-//default pos
-let ArrowRight = false;
-let ArrowLeft = false;
-let isJump = false;
-let onGround = true;
-
-
-function Player(){
-    function updatePlayer(){
-        ctx = canvas.context;
-        window.addEventListener("keydown", function(event) { switch(event.code) {
-            case "ArrowLeft":
-                ArrowLeft = true;
-                break;
-            case "ArrowRight":
-                ArrowRight = true;
-                break;
-            case "ArrowUp": 
-                if (!(isJump)){
-                    isJump = true;
-                    onGround = false;
-                }                
-                break;     
-        }}, true);
-        window.addEventListener("keyup", function(event) {
-            switch(event.code) {
-                case "ArrowLeft":
-                    ArrowLeft = false;   
-                    break;            
-                case "ArrowRight":
-                    ArrowRight = false;    
-                    break; 
-                case "ArrowUp":
-                    break;
-            }
-        }, true);
-        if (ArrowRight){
-            x += x_vel;
-        } else if (ArrowLeft){
-            x -= x_vel;
-        }
-        
-        //console.log(onGround, isJump)
-        if (isJump && !(onGround)){
-            if(y_vel > -11){
-                y -= y_vel;
-                y_vel -= 1; 
-            }
-        }else if (isJump){                       
-            isJump = false;
-            y_vel = 10;  
-        }
-        if (isJump){
-
-            //for (i = 0; i < platforms.length+1; i++) {
-              //  platforms[i].y = platform_y
-              //  platforms[i].x = platform_x
-              //  if (y === platform_y || y === platform_x){
-                //  onGround = true;
-                //  isJump = false;
-                //  y_vel = 1;
-              //}
-              //}
-            if (y === 695){
-                onGround = true;
-                isJump = false;
-                y_vel = 10;
-            }
-        }
-    }
-    function drawPlayer(){
-        ctx.clearRect(0,0,800,800);
-        //del clear after from this 
-        ctx.fillStyle = "red";
-        ctx.fillRect(x,y,15,15);
-    }
-    updatePlayer();
-    drawPlayer();
+let canvas = {
+   canvas: document.createElement("canvas"),
+   start: function () {
+       this.canvas.width = 800;
+       this.canvas.height = 800;
+       this.context = this.canvas.getContext("2d");
+       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+       this.coinInterval = 0;
+       this.game = setInterval(game, 20);
+      let ctx = canvas.context;
+   },
+   stop : function() {
+       clearInterval(this.game);
+   }
 }
 
-function Platform(){
-    //del clear after from this 
-    ctx.fillStyle = "black";
-    ctx.fillRect(0,710,10,10);
+let ctx = canvas.context;
+
+function drawPlayer(x,y,width,height) { 
+         ctx.beginPath();
+         ctx.rect(x,y,width,height);
+         ctx.fillStyle = 'brown';
+         ctx.fill();
+      }
+
+function game() {
+   this.x = 100
+   this.y = 100
+   this.width = 150
+   this.height = 150
+   vel = 10
+   drawPlayer(this.x, this.y, this.width, this.height)
+   
+   window.addEventListener("keydown", function(event) {   switch(event.code) {
+       case "KeyS":
+       case "ArrowDown":
+           //Movement logic down
+           break;
+       case "KeyW":
+       case "ArrowUp":
+           //Movement logic up
+           break;
+       case "KeyA":
+       case "ArrowLeft":
+           this.x -= vel
+           break;
+       case "KeyD":
+       case "ArrowRight":
+           this.x += vel
+           break;
+      }
+   }, true);
+
+   window.addEventListener("keyup", function(event) {
+      switch(event.code) {
+          case "KeyS":
+          case "ArrowDown":
+          case "KeyW":
+          case "ArrowUp":
+              //Movement logic vertical stop
+              break;
+          case "KeyA":
+          case "ArrowLeft":
+          case "KeyD":
+          case "ArrowRight":
+              //Movement logic horizontal stop
+              break;
+      }
+   }, true);
 }
-
-function Map(){
-    this.row1 = [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x]; 
-    this.row2 = [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x];
-    this.row3 = [x,x,x,x,x,x,x,x,x,x,x,x,x,x,o,o,x,x,x];
-    this.row4 = [x,x,x,x,x,x,x,x,x,o,o,x,x,x,x,x,x,x,x];
-    this.row5 = [x,x,o,o,o,o,o,o,x,x,x,x,x,x,x,x,x,x,x];
-    this.row6 = [x,x,x,x,x,x,x,x,x,x,x,u,u,u,x,x,x,x,x];
-    
-    // u = lava, x = nothing, o = platform
-    
-}
-
-
-function game(){
-    Player();
-    Platform();
-    
-
- }
+game()
 
 
 
+//adsfsdafawefwaefa
+//adsfawefawfawefawf
 
-
-
-
-
-
+//asdfawefaewfweaf
